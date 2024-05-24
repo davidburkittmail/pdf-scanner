e8895e70308f7cfceac6ed4a7988a6302901c2b0
David <davidburkitt@mail.com>
2024-05-24 14:44:08 +0100

    Update functionality

--- pdf-scanner.py
+++ pdf-scanner.py
@@ -1,7 +1,8 @@
-import PyPDF2, re, sys, urllib.request, os
+import PyPDF2, re, requests, sys, urllib.request, os
+from bs4 import BeautifulSoup
 
 def search_pdf(pdf,term):
-    print("Scanning "+pdf+" for "+term)
+    #print("Scanning "+pdf+" for "+term)
     reader = PyPDF2.PdfReader(pdf)
     num_pages = len(reader.pages)
     match = False
@@ -13,18 +14,46 @@ def search_pdf(pdf,term):
     return match
 
 def download_pdf(pdfUrl):
-    uri, fileName = os.path.split(pdfUrl)
-    print("Downloading from "+uri+" to "+fileName)
+    fileName = os.path.split(pdfUrl)[1]
+    #print("Downloading "+pdfUrl)
     urllib.request.urlretrieve(pdfUrl, fileName)
     return fileName
 
+def find_anchors(aUrl,aTag,aClass):
+    html = requests.get(aUrl).text
+    page = BeautifulSoup(html,features="html.parser")
+    urls = []
+    for div in page.find_all(aTag, class_=aClass):
+        a = div.find('a') 
+        try:
+            if 'href' in a.attrs: 
+                url = a.get('href') 
+                urls.append(url) 
+        except: 
+            pass
+    return urls
+
 def main():
-    pdfUrl = sys.argv[1]
+    site = sys.argv[1]
     searchTerm = sys.argv[2]
-    pdfFile = download_pdf(pdfUrl)
-    if search_pdf(pdfFile,searchTerm):
-        print("Match - "+pdfFile)
-    os.remove(pdfFile)
+    depth = int(sys.argv[3])
+    index = 1
+    baseUrl = os.path.split(site)[0]
+    while index <= depth:
+        siteAndPage = site + "?page=" + str(index)
+        print("Scanning " + siteAndPage)
+        urls = find_anchors(siteAndPage,"div","gem-c-document-list__item-title")
+        for url in urls:
+            #print(baseUrl+url)
+            files = find_anchors(baseUrl+url,"span","attachment-inline")
+            #print(files[0])
+            for file in files:
+                pdfFile = download_pdf(file)
+                if search_pdf(pdfFile,searchTerm):
+                    print("Match - "+file)
+                os.remove(pdfFile)
+        index = index + 1
+    
 
 if __name__ == "__main__":
     main()
